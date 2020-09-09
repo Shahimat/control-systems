f6284d59bae540d95de121ba97be630afdd88694
Геворг Нахапетян <g_nahapetan@Air-Gevorg.Dlink>
2020-09-10 01:59:49 +0300

    feat[REST]: /person DELETE added

--- restservice/src/person/person.controller.ts
+++ restservice/src/person/person.controller.ts
@@ -1,20 +1,22 @@
-import { Controller, Get, Post, Body, Res, HttpStatus } from '@nestjs/common';
+import { Controller, Get, Post, Delete, Param, Body, Res, HttpStatus } from '@nestjs/common';
 import { pool } from './../pool';
 import { Response } from 'express';
 
-@Controller('person')
+const DB_NAME: string = 'person';
+
+@Controller(DB_NAME)
 export class PersonController {
   
     @Get()
     findAll(@Res() response: Response) {
-        let query: string = `SELECT * FROM person;`;
+        let query: string = `SELECT * FROM ${DB_NAME};`;
         console.log(query);
         pool.query(query, (err, res) => {
             if (err) {
                 console.log(err);
                 response.status(HttpStatus.INTERNAL_SERVER_ERROR).send(err);
             } else {
-                console.log('GET person OK');
+                console.log(`GET ${DB_NAME} OK`);
                 response.status(HttpStatus.OK).json(res.rows);
             }
         });
@@ -24,20 +26,35 @@ export class PersonController {
     create(@Body() values: any, @Res() response: Response) {
         console.log(values);
         if (values.name && typeof(values.name) == "string") {
-            let query: string = `INSERT INTO person (name) values ('${values.name}');`;
+            let query: string = `INSERT INTO ${DB_NAME} (name) values ('${values.name}');`;
             console.log(query);
             pool.query(query, (err, res) => {
                 if (err) {
                     console.log(err);
                     response.status(HttpStatus.INTERNAL_SERVER_ERROR).send(err);
                 } else {
-                    console.log('GET person OK');
-                    response.status(HttpStatus.OK).send('person added');
+                    console.log(`GET ${DB_NAME} OK`);
+                    response.status(HttpStatus.OK).send(`${DB_NAME} added`);
                 }
             });
         } else {
-            response.status(HttpStatus.INTERNAL_SERVER_ERROR).send(`ERROR: values.name = ${values.name}`);
+            response.status(HttpStatus.INTERNAL_SERVER_ERROR).send(`ERROR: values = \n${values}`);
         }
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string, @Res() response: Response) {
+        let query: string = `DELETE FROM ${DB_NAME} WHERE name = '${id}';`;
+        console.log(query);
+        pool.query(query, (err, res) => {
+            if (err) {
+                console.log(err);
+                response.status(HttpStatus.INTERNAL_SERVER_ERROR).send(err);
+            } else {
+                console.log(`DELETE ${id} from ${DB_NAME} OK`);
+                response.status(HttpStatus.OK).send(`${id} deleted`);
+            }
+        });
     }
 
 }
