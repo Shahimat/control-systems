ff1faf6f56a43c226c7e1a902076dfa09053adcb
Геворг Нахапетян <g_nahapetan@Air-Gevorg.Dlink>
2020-09-10 22:27:32 +0300

    feat[REST]: /contract POST added

--- restservice/src/routes/contract.controller.ts
+++ restservice/src/routes/contract.controller.ts
@@ -2,21 +2,59 @@ import { Controller, Get, Post, Delete, Put, Param, Body, Res, HttpStatus } from
 import { pool } from './pool';
 import { Response } from 'express';
 
-const DB_NAME: string = 'contract';
+const DB_TABLE_NAME: string = 'contract';
 
-@Controller(DB_NAME)
+function getQuery (oValue: any): string {
+    let names: string = '';
+    let values: string = '';
+    for (let key in oValue) {
+        if (oValue[key] != undefined && oValue[key] != null) {
+            if (oValue[key][0] == '@') {
+                oValue[key] = oValue[key].slice(1).replace(/(\d+).(\d+).(\d+)/g,'$3-$2-$1');
+            }
+            names  += `${key},`;
+            values += `'${oValue[key]}',`;
+        }
+    }
+    return `INSERT INTO ${DB_TABLE_NAME} (${
+        names.slice(0,names.length - 1)
+    }) VALUES (${
+        values.slice(0,values.length - 1)
+    });`;
+}
+
+function checkQuery (oValue: any): boolean {
+    return oValue.ContractNumber && typeof(oValue.ContractNumber) == "string" &&
+    oValue.ContractDate          && typeof(oValue.ContractDate)   == "string" &&
+    oValue.ContractType          && typeof(oValue.ContractType)   == "string" &&
+    oValue.id_provider           && typeof(oValue.id_provider)    == "number" &&
+    oValue.id_payer              && typeof(oValue.id_payer)       == "number" &&  
+    oValue.currency              && typeof(oValue.currency)       == "string" &&
+    oValue.filepath              && typeof(oValue.filepath)       == "string";
+}
+
+async function findPosition (sTableName, sFindFieldName, sFieldName, sFieldValue: string) {
+    if (!sTableName || !sFindFieldName || !sFieldName || !sFieldValue) {
+        throw 'Field undefined';
+    }
+    let query: string = `SELECT ${sTableName}.${sFindFieldName} FROM ${sTableName} WHERE ${sFieldName} = '${sFieldValue}';`;
+    console.log(query);
+    return await pool.query(query);
+}
+
+@Controller(DB_TABLE_NAME)
 export class ContractController {
   
     @Get()
     findAll(@Res() response: Response) {
-        let query: string = `SELECT * FROM ${DB_NAME};`;
+        let query: string = `SELECT * FROM ${DB_TABLE_NAME};`;
         console.log(query);
         pool.query(query, (err, res) => {
             if (err) {
                 console.log(err);
                 response.status(HttpStatus.INTERNAL_SERVER_ERROR).send(err);
             } else {
-                console.log(`GET ${DB_NAME} OK`);
+                console.log(`GET ${DB_TABLE_NAME} OK`);
                 response.status(HttpStatus.OK).json(res.rows);
             }
         });
@@ -24,34 +62,48 @@ export class ContractController {
 
     @Post()
     create(@Body() values: any, @Res() response: Response) {
-        console.log(values);
-        if (values.name && typeof(values.name) == "string") {
-            let query: string = `INSERT INTO ${DB_NAME} (name) values ('${values.name}');`;
-            console.log(query);
-            pool.query(query, (err, res) => {
-                if (err) {
-                    console.log(err);
-                    response.status(HttpStatus.INTERNAL_SERVER_ERROR).send(err);
-                } else {
-                    console.log(`GET ${DB_NAME} OK`);
-                    response.status(HttpStatus.OK).send(`${DB_NAME} added`);
-                }
-            });
-        } else {
-            response.status(HttpStatus.INTERNAL_SERVER_ERROR).send(`ERROR: values = \n${values}`);
-        }
+        findPosition ('Person', 'id', 'Name', values.provider)
+        .then(res => {
+            if (res.rows == undefined || res.rows.length == 0 || res.rows[0].id == undefined) {
+                throw `provider = ${values.provider} not found`;
+            }
+            values.id_provider = res.rows[0].id;
+            values.provider    = null;
+            return findPosition ('Person', 'id', 'Name', values.payer);
+        })
+        .then(res => {
+            if (res.rows == undefined || res.rows.length == 0 || res.rows[0].id == undefined) {
+                throw `payer = ${values.payer} not found`;
+            }
+            values.id_payer = res.rows[0].id;
+            values.payer    = null;
+            if (checkQuery(values)) {
+                console.log(values)
+                return pool.query(getQuery(values));
+            } else {
+                throw values;
+            }
+        })
+        .then(res => {
+            console.log('in');
+            response.status(HttpStatus.OK).send(`${DB_TABLE_NAME} added`);
+        })
+        .catch(function(e) {
+            response.status(HttpStatus.INTERNAL_SERVER_ERROR).send(e);
+            console.log(e);
+        })
     }
 
     @Delete(':id')
     remove(@Param('id') id: string, @Res() response: Response) {
-        let query: string = `DELETE FROM ${DB_NAME} WHERE name = '${id}';`;
+        let query: string = `DELETE FROM ${DB_TABLE_NAME} WHERE name = '${id}';`;
         console.log(query);
         pool.query(query, (err, res) => {
             if (err) {
                 console.log(err);
                 response.status(HttpStatus.INTERNAL_SERVER_ERROR).send(err);
             } else {
-                console.log(`DELETE ${id} from ${DB_NAME} OK`);
+                console.log(`DELETE ${id} from ${DB_TABLE_NAME} OK`);
                 response.status(HttpStatus.OK).send(`${id} deleted`);
             }
         });
@@ -59,17 +111,17 @@ export class ContractController {
 
     @Put(':id')
     update(@Param('id') id: string, @Res() response: Response, @Body() values: any) {
-        let query: string = `UPDATE ${DB_NAME} SET name = '${values.name}' WHERE name = '${id}';`;
+        let query: string = `UPDATE ${DB_TABLE_NAME} SET name = '${values.name}' WHERE name = '${id}';`;
         console.log(query);
         pool.query(query, (err, res) => {
             if (err) {
                 console.log(err);
                 response.status(HttpStatus.INTERNAL_SERVER_ERROR).send(err);
             } else {
-                console.log(`DELETE ${id} from ${DB_NAME} OK`);
+                console.log(`DELETE ${id} from ${DB_TABLE_NAME} OK`);
                 response.status(HttpStatus.OK).send(`${id} updated`);
             }
         });
     }
-
+    
 }
